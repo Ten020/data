234cc66291320d03a2f9f522ac7142295c3299d4
Ten020 <133095583+Ten020@users.noreply.github.com>
2023-11-17 13:49:07 +0100

    Delete datebase directory

--- datebase/verwerking.php
+++
@@ -1,8 +0,0 @@
-<?php
-//De informatie van het formulier op form.html komt terecht in de variabele $_POST
-// $_POST is een array 
-// en die kunnen we uitprinten met de functie print_r();
-
-print_r($_GET);
-
-//echo "$_POST";
