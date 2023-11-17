f4d17d13a021c90a08006279d0ead1bb2c75e168
Ten020 <133095583+Ten020@users.noreply.github.com>
2023-11-17 13:25:36 +0100

    Add files via upload

---
+++ datebase/verwerking.post.php
@@ -0,0 +1,8 @@
+<?php
+//De informatie van het formulier op form.html komt terecht in de variabele $_POST
+// $_POST is een array 
+// en die kunnen we uitprinten met de functie print_r();
+
+print_r($_POST);
+
+//echo "$_POST";
